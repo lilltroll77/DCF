b20fa690d2720694ca02ac09c7a28b35e34e1e59
Mikael <micke.lilltroll@hotmail.com>
2011-03-08 23:09:12 +0100

    updated

--- DCF.m
+++ DCF.m
@@ -1,4 +1,8 @@
-% Digital Current Feedback GUI for MATLAB <-> XMOS
+% Digital Current Feedback GUI for MATLAB 2010a <-> XMOS
 
-dll_path='C:\\Thesycon\\TUSBAudio_v1.22.0\\EvaluationKit\\DriverInstaller\\release\\';
+dll_path = 'C:\Thesycon\TUSBAudio_v1.22.0\EvaluationKit\DriverInstaller\release\';
+h_path = 'C:\Thesycon\TUSBAudio_v1.22.0\EvaluationKit\DriverInstaller\release\DCF\';
+
+[NOTFOUND, WARNINGS] = loadlibrary([dll_path 'tusbaudioapi.dll'],[h_path 'tusbaudioapi.h'] );
+libfunctions('tusbaudioapi')
 
